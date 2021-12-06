7aa578197a103209937f91b1a616038100b0a6c9
CiechanM <m.ciechan12@gmail.com>
2021-12-06 21:55:42 +0100

    Created encrypting scheme

---
+++ client/GUI/encrypting/encrypting.py
@@ -0,0 +1,12 @@
+import enum
+
+class Encrypting:
+    def __init__(self):
+          encrypting_type = ""  #szyfr wybrany w gui
+            
+    
+class Cipher(enum.Enum):
+    NO_CIPHER = 1
+    UBCHI = 2
+    INNY = 3
+

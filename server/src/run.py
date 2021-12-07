d4c07cc588e1ca857f0f29249b2c7da4c9f5c71b
Dzemoro <dellorpl@gmail.com>
2021-12-07 22:46:26 +0100

    New server and changes in common classes

--- server/src/run.py
+++ server/src/run.py
@@ -1,30 +1,10 @@
+from sys import path
+from typing import List
+if '' not in path:
+    path.append('')
 import os
-from server.src.server import Server
+from server import Server
 
-file_problem = False
-ip_regex = "^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$"
-
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'config.txt')
-      
-try:
-    file = open(filename, "r")
-    content = file.read().splitlines()
-    ip = content[0]
-    tcp_port = content[1]
-    file.close()
-    if(ip.lower() != 'auto' and ip != '' and not re.search(ip_regex, ip)):
-        raise ValueError('Wrong IP address')
-    if(tcp_port.lower() != 'auto' and tcp_port != '' and (int(tcp_port) > 65535 or int(tcp_port) < 1)):
-        raise ValueError('Wrong port')
-except ValueError as er:
-    print("Config error: "+str(er)+"\nProper config.txt file:\n   1st line is server IP (you may use 'auto' or '')\n   2nd line is server port to listen on (you may use 'auto' or '')")
-    input()
-    file_problem = True
-except:
-    print("Missing proper config.txt file:\n   1st line is server IP (you may use 'auto' or '')\n   2nd line is server port to listen on (you may use 'auto' or '')")
-    input()
-    file_problem = True
-
-if(not file_problem):
-    server = Server(ip, tcp_port)
+if __name__ == "__main__":
+    server = Server('127.0.0.1', 60000)
+    server.run()
--- server/src/server.py
+++ server/src/server.py
@@ -1,131 +1,49 @@
 from sys import path
-from typing import List
 if '' not in path:
     path.append('')
 
-import socket
-from threading import Thread
-from contextlib import closing
-import re, sys, time, os
+import socket, threading, ssl
+import os
 from common.message_type import MessageType
 from common.user import User
-from enum import Enum
-        
-class Server:
-    def __init__(self, ip, tcp_port):
-        if(ip.lower() == 'auto' or ip == ''):
-            self.ip = socket.gethostbyname(socket.gethostname())
-        else:
-            self.ip = ip
+from message import Message
 
-        if(tcp_port.lower() == 'auto' or tcp_port == ''):
-            self.server_tcp_port = 0
-        else:
-            self.server_tcp_port = int(tcp_port)
-        
-        self.userList = []
+class Server(object):
+    def __init__(self, ip, port) -> None:
+        super().__init__()
 
-        self.tcp_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_s.bind((self.ip, self.server_tcp_port))
-        self.server_tcp_port = self.tcp_s.getsockname()[1]
-
-        self.udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_s.bind((self.ip, 0))
-
-        print("Server is listening on "+self.ip+':'+str(self.server_tcp_port)+" ...")
-        self.userConnections()
-
-    def userConnections(self):
-            self.tcp_s.listen(25)
-            while True:
-                conn, address = self.tcp_s.accept()
-                data = conn.recv(1024) #messages: JOIN Nick; AWLI; LEAV; GIVE Nick;
-                decoded = data.decode('UTF-8')
-                message = decoded.split()
-                if(message[0] == 'JOIN' and len(message[1]) > 0):
-                    
-                    user = User(conn, message[1], address, int(message[2]))
-                    self.userList.append(user)
-
-                    #create udp socket for user
-                    #user_udp_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    #user_udp_s.bind((self.ip, 0))
-
-                    #send OK with udp port for audio communication
-                    conn.send(bytes('OK', 'UTF-8'))
-                    #print("\n"+user.udpAddr[0]+" User "+user.name+" joined server")
-                    #print("  TCP: "+str(user.tcpAddr[1])+" <-> "+str(self.server_tcp_port))
-                    #print("  UDP: "+str(user.udpAddr[1])+" <-> "+str(user_udp_s.getsockname()[1]))
-                    
-                    Thread(target=self.newConnection, args=(conn, user)).start()
-                    #Thread(target=self.audioStreaming, args=(user_udp_s, user)).start()
-                else:
-                    print('\nReceived bad data: '+decoded+' from: '+str(address[0])+':'+str(address[1]))
-                    conn.send(bytes('BAD DATA', 'UTF-8'))
-                    conn.close()
-
-    def newConnection(self, conn, user):
-            while True:
-                try:
-                    data = conn.recv(1024)
-                    decoded = data.decode('UTF-8')
-                    message = decoded.split()
-                    if(message[0] == 'LEAV'):
-                        self.userList.remove(user)
-                        conn.send(bytes('BYE', 'UTF-8'))
-                        print("\nUser "+user.name+" disconnected peacefully")
-                        break
-                    elif(message[0] == 'AWLI'):
-                        message = 'LIST'
-                        for u in self.userList:
-                            message += ' '+str(u.name)
-                        conn.send(bytes(message, 'UTF-8'))
-                        
-                        #TODO wysłanie IP wybranego usera
-                    elif(message[0] == 'GIVE' and len(message[1]) > 0):
-                        #szukanie po nazwie usera
-                        #print('give')
-                        message = 'IP'
-                        usrname = message[1]
-                        for u in self.userList:
-                            if (u.name == usrname):
-                                message += ' '+str(u.tcpAddr)
-                        conn.send(bytes(message, 'UTF-8'))
-
-                except socket.error:
-                    try:
-                        print("\nUser "+user.name+" disconnected forcibly")
-                        self.userList.remove(user)
-                        conn.close()
-                        break
-                    except:
-                        break
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((ip, port))
+        self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+        self.context.load_cert_chain(certfile=r"F:\Repositories\testServer\server\src\cert.pem")
+        self.context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
+        self.context.set_ciphers('AES256+ECDH:AES256+EDH')
+        self.sock_address = self.sock.getsockname()
+        self.is_running = True
+        self.active_users = {}
+        self.lock = threading.Lock()
     
-    def identify_message_type(self, data):
-        if isinstance(MessageType[data], MessageType):
-            return MessageType[data]
-        else:
-            raise Exception('Unknown message type')
-    
-    def handle_message(self, type : MessageType, data, conn, address):
-        user = User(conn, '', address)
-        if type is MessageType.JOIN:
-            #user = User(conn, data[1], address)
-            self.userList.append(user)
-            #send OK with udp port for audio communication
-            conn.send(bytes('OK', 'UTF-8'))
-            Thread(target=self.newConnection, args=(conn, user)).start()
-        elif type is MessageType.LIST:
-            message = 'LIST'
-            for u in self.userList:
-                message += ':'+str(u.name)
-            conn.send(bytes(message, 'UTF-8'))
-        elif type is MessageType.QUIT:
-            self.userList.remove(user)
-            conn.send(bytes('BYE', 'UTF-8'))
-            print("\nUser "+user.name+" disconnected peacefully")
-        else:
-            print('\nReceived invalid data from: '+str(address[0])+':'+str(address[1]))
-            conn.send(bytes('BAD DATA', 'UTF-8'))
-            conn.close()
+    def run(self):
+        self.sock.listen(50)
+        
+        while self.is_running:
+            try:
+                conn, addr = self.sock.accept()
+                wrap = self.context.wrap_socket(conn, server_side=True)
+                threading.Thread(target=self.message_handler, args=(wrap, addr,)).start()
+            except socket.error as err:
+                print(err)
+                break
 
+    def message_handler(self, conn, addr):
+        user = User(conn, addr)
+        msg = Message('UTF-8')
+        data = msg.receive(conn)
+        if len(data) > 1:
+            type = msg.identify_message_type(data[0])
+            if type is MessageType.JOIN:
+                self.active_users[data[1]] = addr
+                msg.send(MessageType.SPOX.name, conn)
+            else:
+                msg.send(MessageType.DENY.name, conn)
